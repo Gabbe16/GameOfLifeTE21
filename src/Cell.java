0cc676abcf717661c670dc25a9472a7b8e4ef84e
Gabbe16 <94972340+Gabbe16@users.noreply.github.com>
2023-10-24 13:35:44 +0200

    Added Cell class + Rules class

--- src/Cell.java
+++ src/Cell.java
@@ -1,2 +1,21 @@
 public class Cell {
+    private int x;
+    private int y;
+    private boolean isAlive;
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    public void setAlive(boolean alive) {
+        isAlive = alive;
+    }
 }
